f2c5a2ac44d05180ec6d3dced28985aaac49fbc1
Alexander Samilovskikh <alex.kmoxville@gmail.com>
2022-01-30 14:14:05 +0300

    Lesson 1

---
+++ Lesson1/Models/TemperatureStamp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lesson1.Models
+{
+    public class TemperatureStamp
+    {
+        public long Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal TemperatureC { get; set; }
+
+        public decimal TemperatureF => TemperatureC + (decimal)273.15;
+    }
+}
---
+++ Lesson1/Models/TemperatureStampsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lesson1.Models
+{
+    public class TemperatureStampsContext : DbContext
+    {
+        public TemperatureStampsContext(DbContextOptions opt)
+            : base(opt)
+        {
+
+        }
+
+        public DbSet<TemperatureStamp> TemperatureStamps { get; set; } = null!;
+    }
+}
